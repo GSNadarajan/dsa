3a6131543c4bc4ba7a803c6e62857bc6f78ec2f5
GSNadarajan <nadarajangs31@gmail.com>
2023-10-20 01:09:06 +0530

    feat: STACK

---
+++ day1/Main.java
@@ -0,0 +1,38 @@
+package day1;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        // STACK
+        // LAST IN FIRST OUT DATASTRUCTURES
+        // STORES OBJECT IN A VERTICAL ORDER (eg: books in a shelf)
+        // PUSH() to add to the top
+        // Pop() to remove from the top
+
+        Stack<String> stack = new Stack<String>();
+        System.out.println(stack.empty());
+
+        stack.push("book1");
+        stack.push("book2");
+        stack.push("book3");  //To add a new element into the stack
+
+        System.out.println(stack.empty()); //To check whether the stack is empty or not
+
+        System.out.println(stack);
+
+        stack.pop(); //To remove the top element 
+
+        System.out.println(stack.peek()); //To get the top element 
+        System.out.println(stack.search("book2")); //To search the particular item from the stack
+
+        for(int i=0; i < 100000000; i++){
+            stack.push("book4");      // It throws heap space we can't do like this in stack
+
+        }
+        System.out.println(stack);
+
+    }
+}
